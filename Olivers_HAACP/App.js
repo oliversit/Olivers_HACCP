9f84baa6906173dad471202fe6e0945f32492ddd
bbranch-olibas <78439505+bbranch-olibas@users.noreply.github.com>
2021-03-09 16:32:07 -0800

    Add files via upload

--- Olivers_HAACP/App.js
+++ Olivers_HAACP/App.js
@@ -16,6 +16,7 @@ app.set("view engine", "ejs");
 
 app.use('/Frontend', BringtoFront);
 
+
 var connection = mysql.createConnection({
 
   host     : 'localhost',
@@ -28,22 +29,21 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
-
 app.post('/data', function(req, res){
-
+var user= req.body.username;
     connection.query("INSERT INTO `Submitted_Forms` (Date,Form_Name, Corection_ID, Column_1, Column_2, Column_3, Column_4, Column_5, Column_6, Column_7, Column_8, Column_9, Column_10, Column_11, Column_12, Column_13, Column_14, Column_15, Column_16, Column_17, Column_18, Column_19, Column_20 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )", [req.body.Date, req.body.Form_Name, req.body.Corection_ID, req.body.Column_1, req.body.Column_2, req.body.Column_3, req.body.Column_4, req.body.Column_5, req.body.Column_6, req.body.Column_7, req.body.Column_8, req.body.Column_9, req.body.Column_10, req.body.Column_11, req.body.Column_12, req.body.Column_13, req.body.Column_14, req.body.Column_15, req.body.Column_16, req.body.Column_17, req.body.Column_18, req.body.Column_19, req.body.Column_20 ], function(err, result){
         if(err) throw err;
             console.log("1 record inserted");
         });
     
-	res.sendFile(__dirname + '/public/Dashboard.html');
+res.render('Dashboard', { title: 'Dashboard', userName: user});
 	
 });
 
 
 
 app.post('/List', function(req, res){
-
+var user= req.body.username;
     connection.query("INSERT INTO `Item_Maint` (Type,Name) VALUES (?,?)", [req.body.Type, req.body.Name], function(err, result){
         if(err) throw err;
             console.log("1 record inserted");
@@ -52,7 +52,7 @@ app.post('/List', function(req, res){
 	    connection.query('SELECT * FROM Item_Maint Where Type=?', [req.body.Type],function (err, data, fields) {
 		var form=req.body.Type
     if (err) throw err;
-    res.render('List_Edit', { title: 'List_Edit', userData: data, formName: form});
+    res.render('List_Edit', { title: 'List_Edit', userData: data, formName: form, userName: user});
 		
   
 
@@ -74,67 +74,6 @@ app.get('/close', function(req,res){
     res.send('Database connection successfully closed');
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/view', function(req, res) {
-
-    var testimonials;
-
-    // Running database queries
-    connection.query('SELECT * FROM Submitted_Forms', function(err, rows, fields) {
-        if (!err) {
-            testimonials = rows;
-            console.log(testimonials); // THIS WORKS
-        }
-        if (err) {
-            console.log('Error performing database query.');
-        }
-    });
-
-
-  res.render('index', {
-      title: 'Title',
-      description: 'Description in here.',
-      testimonials: testimonials // THIS RETURNS UNDEFINED
-  });
-});
-
-
-
-
-
-app.get('/close', function(req,res){
-  connection.end((err) => {
-    if (err) {
-      res.send('There is some error in closing the database');
-      return console.error(err.message);
-    }
-    console.log('Closing the database connection.');
- 
-  });
-	
-	
-});
-
-
-
-
-
-
-
-
-
-
 
 
 
--- Olivers_HAACP/routes/Frontend.js
+++ Olivers_HAACP/routes/Frontend.js
@@ -20,22 +20,22 @@ router.get('/user-list', function(req, res, next) {
 
 
 	router.post('/List_Edit', function(req, res, next) {
-
+var user= req.body.username;
     connection.query('SELECT * FROM Item_Maint Where Type=?', [req.body.Type],  function(err, data, fields) {
 		var form=req.body.Type
     if (err) throw err;
-    res.render('List_Edit', { title: 'List_Edit', userData: data, formName: form});
+    res.render('List_Edit', { title: 'List_Edit', userData: data, formName: form, userName: user});
 	
   });
 
 });
 
 	router.post('/List_Edit_Item', function(req, res, next) {
-
+var user= req.body.username;
     connection.query('SELECT * FROM Item_Maint Where Type=? and Id=?', [req.body.Type, req.body.Id],  function(err, data, fields) {
 		var form=req.body.Type
     if (err) throw err;
-    res.render('List_Edit_Item', { title: 'List_Edit_Item', userData: data, formName: form});
+    res.render('List_Edit_Item', { title: 'List_Edit_Item', userData: data, formName: form, userName: user});
 	
   });
 
@@ -46,10 +46,7 @@ router.get('/user-list', function(req, res, next) {
 
 
 	router.post('/List_Delete', function(req, res, next) {
-	
-	
-	
-	
+	var user= req.body.username;	
     connection.query("Delete From `Item_Maint` Where Id=?", [req.body.Id], function(err, result){
         if(err) throw err;
             console.log("1 record inserted");
@@ -58,7 +55,7 @@ router.get('/user-list', function(req, res, next) {
 	    connection.query('SELECT * FROM Item_Maint Where Type=?', [req.body.Type],function (err, data, fields) {
 		var form=req.body.Type
     if (err) throw err;
-    res.render('List_Edit', { title: 'List_Edit', userData: data, formName: form});
+    res.render('List_Edit', { title: 'List_Edit', userData: data, formName: form, userName: user});
 		
   
 
@@ -71,10 +68,7 @@ router.get('/user-list', function(req, res, next) {
 });
 
 	router.post('/List_Update', function(req, res, next) {
-	
-	
-	
-	
+	var user= req.body.username;	
     connection.query("UPDATE Item_Maint SET Name = ? WHERE Id=?", [req.body.Name, req.body.Id], function(err, result){
         if(err) throw err;
             console.log("1 record inserted");
@@ -83,7 +77,7 @@ router.get('/user-list', function(req, res, next) {
 	    connection.query('SELECT * FROM Item_Maint Where Type=?', [req.body.Type],function (err, data, fields) {
 		var form=req.body.Type
     if (err) throw err;
-    res.render('List_Edit', { title: 'List_Edit', userData: data, formName: form});
+    res.render('List_Edit', { title: 'List_Edit', userData: data, formName: form, userName: user});
 		
   
 
@@ -105,14 +99,11 @@ router.get('/user-list', function(req, res, next) {
 	
 	
 	router.post('/Main_Result', function(req, res, next) {
-	
-	
-	
-	
+	var user= req.body.username;
     connection.query('SELECT * FROM Submitted_Forms Where Form_Name= ? and Date = ?', [req.body.Form_Name, req.body.Date] ,function (err, data, fields) {
 		
     if (err) throw err;
-    res.render('Main_Result', { title: 'Main_Result', userData: data});
+    res.render('Main_Result', { title: 'Main_Result', userData: data, userName: user});
 		
   
 
@@ -126,64 +117,107 @@ router.get('/user-list', function(req, res, next) {
 
 
 
+//-------------------------------------------New Forms ------------------------------------------------------------>
+
+router.post('/Packing_Form_New', function(req, res, next) {
+var user= req.body.username;
+	var FormName= req.body.Form_Name;	
+    connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data, fields) {
+    if (err) throw err;
+    res.render('Packing_Form_New', { title: 'Packing_Form_New',FormData: FormName, userData: data, userName: user });		
+  });	
+});
+
+router.post('/Cooler_Form_New', function(req, res, next) {
+var user= req.body.username;
+	var FormName= req.body.Form_Name;	
+    connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data, fields) {
+    if (err) throw err;
+    res.render('Cooler_Form_New', { title: 'Cooler_Form_New', FormData: FormName, userData: data, userName: user });		
+  });		
+});
+
+router.post('/Processing_Form_New', function(req, res, next) {
+var user= req.body.username;
+	var FormName= req.body.Form_Name;	
+    connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data, fields) {
+    if (err) throw err;
+    res.render('Processing_Form_New', { title: 'Processing_Form_New',FormData: FormName, userData: data, userName: user });	
+  });	
+});
+
+router.post('/Raw_Product_Form_New', function(req, res, next) {
+	var user= req.body.username;
+var user= req.body.username;
+	var FormName= req.body.Form_Name;	
+    connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data2, fields) {
+    if (err) throw err;
+    connection.query('SELECT * FROM Item_Maint Where Type="Vendor"',function (err, data3, fields) {
+    if (err) throw err;
+    res.render('Raw_Product_Form_New', { title: 'Raw_Product_Form_New', FormData: FormName, userData2: data2, userData3: data3, userName: user, userName: user});	
+  });	
+  });
+});
+
+router.post('/Cold_Rooms_Form_New', function(req, res, next) {
+var user= req.body.username;
+	var FormName= req.body.Form_Name;	
+    connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data, fields) {
+    if (err) throw err;
+    res.render('Cold_Rooms_Form_New', { title: 'Cold_Rooms_Form_New', FormData: FormName, userData: data, userName: user });		
+  });		
+});
+
+router.post('/Thermometer_Calibration_Form_New', function(req, res, next) {
+var user= req.body.username;
+	var FormName= req.body.Form_Name;
+    connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data, fields) {
+    if (err) throw err;
+    res.render('Thermometer_Calibration_Form_New', { title: 'Thermometer_Calibration_Form_New',FormData: FormName, userData: data, userName: user });		
+  });		
+});
+
+//-------------------------------------------Submitted Forms ------------------------------------------------------------>
 
 
 
 router.post('/Packing_Form', function(req, res, next) {
+	var user= req.body.username;
     var sql='SELECT * FROM Submitted_Forms Where Form_Name= ? and Date = ? and Id= ?';
     connection.query(sql, [req.body.Form_Name, req.body.Date, req.body.Id] ,function (err, data, fields) {
     if (err) throw err;
-    res.render('Packing_Form', { title: 'Packing_Form', userData: data});
+    res.render('Packing_Form', { title: 'Packing_Form', userData: data, userName: user});
   });
 });
 
 
 router.post('/Cooler_Form', function(req, res, next) {
+	var user= req.body.username;
     var sql='SELECT * FROM Submitted_Forms Where Form_Name= ? and Date = ? and Id= ?';
     connection.query(sql, [req.body.Form_Name, req.body.Date, req.body.Id] ,function (err, data, fields) {
 		
     connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data2, fields) {
     if (err) throw err;
-    res.render('Cooler_Form', { title: 'Cooler_Form', userData: data, userData2: data2});
+    res.render('Cooler_Form', { title: 'Cooler_Form', userData: data, userData2: data2, userName: user});
 		
   });
-		
-		
-		
+					
   });
 });
-
-
-
-router.post('/Cooler_Form_New', function(req, res, next) {
- 
-		
-    connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data, fields) {
-    if (err) throw err;
-    res.render('Cooler_Form_New', { title: 'Cooler_Form_New', userData: data });
-		
-  });
-		
-		
-		
-
-});
-
-
-
-
 
 
 router.post('/Processing_Form', function(req, res, next) {
+	var user= req.body.username;
     var sql='SELECT * FROM Submitted_Forms Where Form_Name= ? and Date = ? and Id= ?';
     connection.query(sql, [req.body.Form_Name, req.body.Date, req.body.Id] ,function (err, data, fields) {
     if (err) throw err;
-    res.render('Processing_Form', { title: 'Processing_Form', userData: data});
+    res.render('Processing_Form', { title: 'Processing_Form', userData: data, userName: user});
   });
 });
 
 
 router.post('/Raw_Product_Form', function(req, res, next) {
+	var user= req.body.username;
     var sql='SELECT * FROM Submitted_Forms Where Form_Name= ? and Date = ? and Id= ?';
     connection.query(sql, [req.body.Form_Name, req.body.Date, req.body.Id] ,function (err, data, fields) {
     if (err) throw err;
@@ -191,7 +225,7 @@ router.post('/Raw_Product_Form', function(req, res, next) {
     if (err) throw err;
     connection.query('SELECT * FROM Item_Maint Where Type="Vendor"',function (err, data3, fields) {
     if (err) throw err;
-    res.render('Raw_Product_Form', { title: 'Raw_Product_Form', userData: data, userData2: data2, userData3: data3});
+    res.render('Raw_Product_Form', { title: 'Raw_Product_Form', userData: data, userData2: data2, userData3: data3, userName: user});
 		
   });
 		
@@ -201,12 +235,13 @@ router.post('/Raw_Product_Form', function(req, res, next) {
 
 
 router.post('/Raw_Product_Form_New', function(req, res, next) {
-
+	var user= req.body.username;
+var user= req.body.username;
     connection.query('SELECT * FROM Item_Maint Where Type="Commodity"',function (err, data2, fields) {
     if (err) throw err;
     connection.query('SELECT * FROM Item_Maint Where Type="Vendor"',function (err, data3, fields) {
     if (err) throw err;
-    res.render('Raw_Product_Form_New', { title: 'Raw_Product_Form_New', userData2: data2, userData3: data3});
+    res.render('Raw_Product_Form_New', { title: 'Raw_Product_Form_New', userData2: data2, userData3: data3, userName: user, userName: user});
 		
   });
 		
@@ -214,33 +249,102 @@ router.post('/Raw_Product_Form_New', function(req, res, next) {
 
 });
 
-
-
-
-
-
-
 router.post('/Cold_Rooms_Form', function(req, res, next) {
+	var user= req.body.username;
     var sql='SELECT * FROM Submitted_Forms Where Form_Name= ? and Date = ? and Id= ?';
     connection.query(sql, [req.body.Form_Name, req.body.Date, req.body.Id] ,function (err, data, fields) {
     if (err) throw err;
-    res.render('Cold_Rooms_Form', { title: 'Cold_Rooms_Form', userData: data});
+    res.render('Cold_Rooms_Form', { title: 'Cold_Rooms_Form', userData: data, userName: user});
   });
 });
 
 
 router.post('/Thermometer_Calibration_Form', function(req, res, next) {
+	var user= req.body.username;
     var sql='SELECT * FROM Submitted_Forms Where Form_Name= ? and Date = ? and Id= ?';
     connection.query(sql, [req.body.Form_Name, req.body.Date, req.body.Id] ,function (err, data, fields) {
     if (err) throw err;
-    res.render('Thermometer_Calibration_Form', { title: 'Thermometer_Calibration_Form', userData: data});
+    res.render('Thermometer_Calibration_Form', { title: 'Thermometer_Calibration_Form', userData: data, userName: user});
   });
+});
+
+//-------------------------------------------Nav Bar ------------------------------------------------------------>
+
+router.post('/Settings', function(req, res, next) {
+	var user= req.body.username;
+    res.render('Settings_Page', { title: 'Settings_Page',  userName: user});
+
+});
+
+router.post('/Maintenance', function(req, res, next) {
+	var user= req.body.username;
+    res.render('Maint_Page', { title: 'Maint_Page',  userName: user});
+
+});
+
+router.post('/Support', function(req, res, next) {
+	var user= req.body.username;
+    res.render('Support_Page', { title: 'Support_Page',  userName: user});
+
+});
+
+
+router.post('/Dashboard', function(req, res, next) {
+	var user= req.body.username;
+    res.render('Dashboard', { title: 'Dashboard', userName: user});
+
+});
+
+router.post('/Log_Out', function(req, res, next) {
+
+    res.redirect('/');
+
 });
 
 
 
 
+
+
+
+
+//-------------------------------------------Log In Procedure ------------------------------------------------------------>
+
+
+
+router.post('/auth', function(req, res) {
+	var user= req.body.username;
+	var password = req.body.password;
+	var FormName= req.body.Form_Name;
+	if (user && password) {
+		connection.query('SELECT * FROM Users WHERE userName = ? AND passwd = ?', [user, password], function(error, results, fields) {
+			
+			if (results.length > 0) {
+					res.render('Dashboard', { title: 'Dashboard',FormData: FormName, userName: user});
+			} else {
+				res.render('Sign_Incorrect', { title: 'Sign_Incorrect', userName: user});
+			}			
+		
+		});
+	}
+});
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
